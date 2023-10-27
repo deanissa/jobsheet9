701d80596d4ea6aa9d7b4ad4ef51c4562c9e905f
deanissa <deanissa604@gmail.com>
2023-10-27 14:29:32 +0700

    nilaiArray setelah dimodifikasi dari pertanyaan

---
+++ ArrayNilai06.java
@@ -0,0 +1,23 @@
+/**
+ * ArrayNilai06
+ */
+import java.util.Scanner;
+ public class ArrayNilai06 {
+ 
+    public static void main (String []args){
+        Scanner sc = new Scanner(System.in);
+
+        int [] nilaiAkhir = new int [10];
+
+        for (int i=0; i<nilaiAkhir.length; i++) {
+        System.out.println ("Masukkan nilai Akhir ke- " +i+" :");
+        nilaiAkhir[i]= sc.nextInt();
+    }
+        for (int i=0; i<nilaiAkhir.length; i++){
+            if (nilaiAkhir[i]>70){
+        System.out.println ("Mahasiswa ke- " +i+"lulus!");
+            }else
+            System.out.println ("Mahasiswa ke-" +i+ "Tidak Lulus!");
+        }
+}
+ }
