cc231c9ebfc52d8ade32e013e046d7e11f4b984f
deanissa <deanissa604@gmail.com>
2023-10-27 20:11:12 +0700

    Linear Search modifikasi dari pertanyaan

--- LinearSearch06.java
+++ LinearSearch06.java
@@ -3,16 +3,25 @@ public class LinearSearch06 {
     public static void main (String []args){
         Scanner sc = new Scanner (System.in);
 
-        int [] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key= 20;
+        int [] arrayInt = new int [10];
+        int key;
         int hasil= 0;
 
-        for (int i = 0; i<arrayInt.length; i++){
-            if (arrayInt[i]==key){
-                hasil = i;
-                break;
-            }
+        for (int i = 0; i< arrayInt.length; i++) {
+            System.out.println("Masukkan elemen array ke-" +i);
+            arrayInt[i]= sc.nextInt();
         }
-    System.out.println("Key ada dalam array pada posisi indeks ke-" +hasil);
-    }
+    
+        System.out.println("Masukkan key yang ingin dicari :");
+            key= sc.nextInt();
+
+            for (int i = 0; i<arrayInt.length; i++){
+            if (arrayInt[i]== key){  
+                hasil = i;
+            System.out.println("Key ada dalam array pada posisi indeks ke-" +hasil);
+            }else 
+            System.out.println("Key ada dalam array tidak ditemukan");
+            break;
+            }
+}
 }
