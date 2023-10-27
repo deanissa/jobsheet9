bc38d7d9063fe1858eac829de487d9f9a22f8ef0
deanissa <deanissa604@gmail.com>
2023-10-27 18:36:32 +0700

    Array Rata Nilai Modifikasi dari pertanyaan

--- ArrayRataNilai06.java
+++ ArrayRataNilai06.java
@@ -3,19 +3,31 @@ public class ArrayRataNilai06 {
  public static void main (String []args){
     Scanner sc = new Scanner (System.in);
     int[] nilaiMhs= new int[10];
-    double total= 0;
+    double totalLulus=0;
+    double totalTdkLulus=0;
+    double Lulus=0;
+    double TdkLulus=0;
+    double rataLulus;
+    double rataTdkLulus;
     double rata2;
 
     for (int i=0; i<nilaiMhs.length; i++){
         System.out.println("Masukkan nilai mahasiswa ke-"+ (i+1)+": ");
         nilaiMhs[i]= sc.nextInt();
     }
-    for (int i = 0; i<nilaiMhs.length; i++) {
-        total += nilaiMhs[i];     
-    }
-    rata2 = total/nilaiMhs.length;
-    System.out.println("Rata-rata = "+rata2);
- }
 
-    
+    for (int i = 0; i<nilaiMhs.length; i++) {
+    if (nilaiMhs[i]>70){
+        totalLulus = totalLulus + nilaiMhs[i];
+       Lulus++;
+     } else{
+        totalTdkLulus = totalTdkLulus + nilaiMhs[i];
+        TdkLulus++;
+        }  
+    }
+    rataLulus = totalLulus/Lulus;
+    rataTdkLulus = totalTdkLulus/TdkLulus;
+    System.out.println("Rata-rata Mahasiswa Lulus = "+rataLulus);
+    System.out.println("Rata-rata Mahasiswa Tidak Lulus = "+rataTdkLulus);
+    }
 }
